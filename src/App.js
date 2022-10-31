ee1f8641bedd8421216bb367d4a0330db34fe4f7
megha <meghamini40@gmail.com>
2022-11-01 01:20:01 +0530

    modified css

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 
+import HomePage from "./Components/HomePage/HomePage.js";
 function App() {
   return (
-    <></>
+    <BrowserRouter >
+      <Routes>
+        <Route path='/' element={<HomePage />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
--- src/Components/Hero/Hero.js
+++ src/Components/Hero/Hero.js
@@ -1,5 +1,9 @@
 import "./styles.css";
-import { AiOutlineCaretDown } from "react-icons/ai";
+import Input from "./Input";
+import Icons from "./Icons";
+import Location from "../../Image/Location.svg";
+import Search from "../../Image/Search.svg";
+import DownAngle from "../../Image/DownAngle.svg";
 function Hero() {
 
     return (
@@ -9,22 +13,14 @@ function Hero() {
                 <h1>Discover the best food & drinks in Bhopal</h1>
                 <div className="searchBar">
                     <div className="dropdown">
-                        <i><svg xmlns="http://www.w3.org/2000/svg" fill="#FF7E8B" width="20" height="20" viewBox="0 0 20 20" aria-labelledby="icon-svg-title- icon-svg-desc-" role="img" class="sc-rbbb40-0 iRDDBk"><title>location-fill</title><path d="M10.2 0.42c-4.5 0-8.2 3.7-8.2 8.3 0 6.2 7.5 11.3 7.8 11.6 0.2 0.1 0.3 0.1 0.4 0.1s0.3 0 0.4-0.1c0.3-0.2 7.8-5.3 7.8-11.6 0.1-4.6-3.6-8.3-8.2-8.3zM10.2 11.42c-1.7 0-3-1.3-3-3s1.3-3 3-3c1.7 0 3 1.3 3 3s-1.3 3-3 3z"></path></svg>
-                            <title>location</title>
-                            <path d="M10.2 0.42c-4.5 0-8.2 3.7-8.2 8.3 0 6.2 7.5 11.3 7.8 11.6 0.2 0.1 0.3 0.1 0.4 0.1s0.3 0 0.4-0.1c0.3-0.2 7.8-5.3 7.8-11.6 0.1-4.6-3.6-8.3-8.2-8.3zM10.2 11.42c-1.7 0-3-1.3-3-3s1.3-3 3-3c1.7 0 3 1.3 3 3s-1.3 3-3 3z"></path>
-                        </i>
-                        <input disabled placeholder=" Bhopal" className="location" />
-                        <i ><svg xmlns="http://www.w3.org/2000/svg" fill="#4F4F4F" width="12" height="12" viewBox="0 0 20 20" aria-labelledby="icon-svg-title- icon-svg-desc-" role="img" class="sc-rbbb40-0 ezrcri"><title>down-triangle</title><path d="M20 5.42l-10 10-10-10h20z"></path></svg>
-                            <title>down-triangle</title>
-                            <path d="M20 5.42l-10 10-10-10h20z"></path></i>
-                        {/* <AiOutlineCaretDown /> */}
+                        <Icons src={Location} />
+                        <Input placeholder="Bhopal" className="location" type="text" disabled={true} />
+                        <Icons src={DownAngle} />
                     </div>
                     <div className="verticalLine"></div>
                     <div className="searchField">
-                        <i><svg xmlns="http://www.w3.org/2000/svg" fill="#828282" width="18" height="18" viewBox="0 0 20 20" aria-labelledby="icon-svg-title- icon-svg-desc-" role="img" class="sc-rbbb40-0 iwHbVQ"><title>Search</title><path d="M19.78 19.12l-3.88-3.9c1.28-1.6 2.080-3.6 2.080-5.8 0-5-3.98-9-8.98-9s-9 4-9 9c0 5 4 9 9 9 2.2 0 4.2-0.8 5.8-2.1l3.88 3.9c0.1 0.1 0.3 0.2 0.5 0.2s0.4-0.1 0.5-0.2c0.4-0.3 0.4-0.8 0.1-1.1zM1.5 9.42c0-4.1 3.4-7.5 7.5-7.5s7.48 3.4 7.48 7.5-3.38 7.5-7.48 7.5c-4.1 0-7.5-3.4-7.5-7.5z"></path></svg>
-                            <title>Search</title>
-                            <path d="M19.78 19.12l-3.88-3.9c1.28-1.6 2.080-3.6 2.080-5.8 0-5-3.98-9-8.98-9s-9 4-9 9c0 5 4 9 9 9 2.2 0 4.2-0.8 5.8-2.1l3.88 3.9c0.1 0.1 0.3 0.2 0.5 0.2s0.4-0.1 0.5-0.2c0.4-0.3 0.4-0.8 0.1-1.1zM1.5 9.42c0-4.1 3.4-7.5 7.5-7.5s7.48 3.4 7.48 7.5-3.38 7.5-7.48 7.5c-4.1 0-7.5-3.4-7.5-7.5z"></path></i>
-                        <input placeholder=" Search for restaurant" type="search" className="searchInput" />
+                        <Icons src={Search} />
+                        <Input placeholder="Search for restaurant" className="searchInput" type="search" disabled={false} />
                     </div>
                 </div>
             </div>
