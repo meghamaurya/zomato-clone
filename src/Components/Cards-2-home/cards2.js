7761d58cf788288c9a0ef1300287adde761d6aa6
megha <meghamini40@gmail.com>
2022-11-01 01:19:35 +0530

    initial commit

---
+++ src/Components/Cards-2-home/cards2.js
@@ -0,0 +1,26 @@
+import Cards from './cards2.json';
+import './style2.css';
+const Cards2 = () => {
+
+    return (
+        <div className="card2-container">
+            <div className="heading">
+                <h3>Collections</h3>
+                <p>Explore curated lists of top restaurants, cafes, pubs, and bars in Bhopal, based on trends</p>
+            </div>
+            {Cards.map((item) => {
+                return (
+                    <div key={item.id} className="card">
+                        <img src={item.image} alt={item.title} className="row2-card" />
+                        <div className='img-text'>
+                            <p>{item.title}</p>
+                            <p>{item.place}</p>
+                        </div>
+                    </div>
+                )
+            })}
+        </div>
+    )
+}
+
+export default Cards2;
