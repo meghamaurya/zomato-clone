b306261b00efc87a06c0364ba9eaea2b8dd158bd
megha <meghamini40@gmail.com>
2022-11-01 01:18:03 +0530

    home page

---
+++ src/Components/HomePage/HomePage.js
@@ -0,0 +1,14 @@
+import Hero from "../Hero/Hero";
+import Cards2 from "../Cards-2-home/cards2";
+import Cards1 from "../Cards-1-home/cards";
+
+const HomePage = () => {
+    return (
+        <div>
+            <Hero />
+            <Cards1 />
+            <Cards2 />
+        </div>
+    )
+}
+export default HomePage;
