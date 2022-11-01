e227adbdac8ea27d05cfc600622780ceb1ccf315
rahulrajput83 <rahulrajput21112@gmail.com>
2022-11-01 10:02:59 +0530

    Fixed conflicts

--- src/Components/HomePage/HomePage.js
+++ src/Components/HomePage/HomePage.js
@@ -1,6 +1,9 @@
 import Hero from "../Hero/Hero";
 import Cards2 from "../Cards-2-home/cards2";
 import Cards1 from "../Cards-1-home/cards";
+import Popular from "../Popular/Popular";
+import Zomato from "../Zomato/Zomato";
+import Footer from "../Footer/Footer";
 
 const HomePage = () => {
     return (
@@ -8,6 +11,9 @@ const HomePage = () => {
             <Hero />
             <Cards1 />
             <Cards2 />
+            <Popular />
+            <Zomato />
+            <Footer />
         </div>
     )
 }
