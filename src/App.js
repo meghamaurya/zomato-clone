cad5e15e34178ef02c100442aedc2f2a922a8138
rahulrajput83 <rahulrajput21112@gmail.com>
2022-11-01 18:33:09 +0530

    Added Explore

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
 import './App.css';
+import Explore from './Components/Explore/Explore';
 import Footer from './Components/Footer/Footer';
 import Popular from './Components/Popular/Popular';
 import Zomato from './Components/Zomato/Zomato';
+import {BrowserRouter, Routes, Route } from 'react-router-dom'
 
 
 function App() {
   return (
-    <>
-      <Popular />
-      <Zomato />
-      <Footer />
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<>
+          <Popular />
+          <Zomato />
+          <Explore />
+          <Footer />
+        </>} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
