279b95a87ea8a2c1dd91878effd0f3b75becbb8e
megha <meghamini40@gmail.com>
2022-11-02 02:18:31 +0530

    modified component, added react icon

--- src/Components/Cards-2-home/cards2.js
+++ src/Components/Cards-2-home/cards2.js
@@ -1,5 +1,9 @@
-import Cards from './cards2.json';
 import './style2.css';
+import Cards from './cards2.json';
+import LinesEllipsis from 'react-lines-ellipsis';
+import responsiveHOC from 'react-lines-ellipsis/lib/responsiveHOC'
+import { GoTriangleRight } from 'react-icons/go';
+const ResponsiveEllipsis = responsiveHOC()(LinesEllipsis);
 
 const Cards2 = () => {
 
@@ -7,19 +11,24 @@ const Cards2 = () => {
         <div className="card2-container">
             <div className="heading">
                 <h3>Collections</h3>
-                <p>Explore curated lists of top restaurants, cafes, pubs, and bars in Bhopal, based on trends</p>
+                <div className='detail'>
+                    <p>Explore curated lists of top restaurants, cafes, pubs, and bars in Bhopal, based on trends</p>
+                    <span className='link-para'>All Collections in Bhopal <GoTriangleRight /></span>
+                </div>
             </div>
-            {Cards.map((item) => {
-                return (
-                    <div key={item.id} className="card">
-                        <img src={item.image} alt={item.title} className="row2-card" />
-                        <div className='img-text'>
-                            <p>{item.title}</p>
-                            <p>{item.place}</p>
+            <div className='cards'>
+                {Cards.map((item) => {
+                    return (
+                        <div key={item.id} className="card">
+                            <img src={item.image} alt={item.title} className="row2-card" />
+                            <div className='img-text'>
+                                <span className='title'><ResponsiveEllipsis text={item.title} maxLine='1' ellipsis="..." trimRight basedOn='letters' /></span>
+                                <span className='place'>{item.place} <GoTriangleRight /></span>
+                            </div>
                         </div>
-                    </div>
-                )
-            })}
+                    )
+                })}
+            </div>
         </div>
     )
 }
