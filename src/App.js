7fdbd58d23b3d083cffa4186f92371d1192603f9
rahulrajput83 <rahulrajput21112@gmail.com>
2022-11-01 09:47:13 +0530

    Updated

--- src/App.js
+++ src/App.js
@@ -1,11 +1,8 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
-import Footer from './Components/Footer/Footer';
-import Popular from './Components/Popular/Popular';
-import Zomato from './Components/Zomato/Zomato';
-
-
 import HomePage from "./Components/HomePage/HomePage.js";
+
+
 function App() {
   return (
     <BrowserRouter >
