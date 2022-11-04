b2116f72653360d33fc2c5b52b3db77cd6600e2e
megha <meghamini40@gmail.com>
2022-11-05 00:59:37 +0530

    merge navbar, card1, card2 component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 import HomePage from "./Components/HomePage/HomePage.js";
+import Navbar from './Components/Navbar/Navbar';
 
 
 function App() {
   return (
     <BrowserRouter >
+      <Navbar />
       <Routes>
         <Route path='/' element={<HomePage />} />
       </Routes>
