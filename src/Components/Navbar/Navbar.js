579339830abc2019279c184cccdb6c9fc358d7cb
megha <meghamini40@gmail.com>
2022-11-04 14:00:25 +0530

    created responsive navbar

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -4,7 +4,7 @@ import './style.css';
 import { IoMdMenu } from "react-icons/io";
 import { RiCloseLine } from "react-icons/ri";
 import { MdOutlineSendToMobile } from 'react-icons/md';
-// import List from './List';
+import List from './List';
 function Navbar() {
     const [isNavExpanded, setIsNavExpanded] = useState(false);
     const [toggleIcon, setToggleIcon] = useState(false);
@@ -14,27 +14,34 @@ function Navbar() {
     }
     return (
         <nav className='navbar'>
-            <button className='hamburger'
-                onClick={handleClick} >
-                {isNavExpanded ? <RiCloseLine className='hamburger-cross' /> : <IoMdMenu className='hamburger-icon' />}
-            </button>
+            <div className='hamburger-heading'>
+                <button className='hamburger'
+                    onClick={handleClick} >
+                    {isNavExpanded ?
+                        <RiCloseLine className='hamburger-cross' />
+                        : <IoMdMenu className='hamburger-icon' />}
+                </button>
+                {isNavExpanded ? <div>
+                    <span className='title'>zomato</span>
+                </div> : ""}
+            </div>
             <div className={isNavExpanded ? 'navigation-menu expanded' : "navigation-menu"}>
                 {!isNavExpanded ? <div className='add'>
                     <MdOutlineSendToMobile /> <span>Get the App</span>
                 </div> : ''}
-                <div className='menu-link'>
-                    {/* <List>Investor Relations</List>
+                <div className='menu-link' >
+                    <List>Investor Relations</List>
                     <List>Add restaurant</List>
                     <List>Log in</List>
-                    <List>Sign up</List> */}
-                    <ul className='ul'>
+                    <List>Sign up</List>
+                    {/* <ul className='ul'>
                         <li className='li'>
                             <Link to="/" className='link'>Investor Relations</Link>
                         </li>
                     </ul>
                     <ul className='ul'>
                         <li className='li'>
-                            <Link to="/" className='link'>Add restaurant</Link>
+                            <Link to="/" className='link' >Add restaurant</Link>
                         </li>
                     </ul>
                     <ul className='ul'>
@@ -46,7 +53,7 @@ function Navbar() {
                         <li className='li'>
                             <Link to="/" className='link'>Sign up</Link>
                         </li>
-                    </ul>
+                    </ul> */}
                 </div>
             </div>
         </nav>
