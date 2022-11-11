593de3802defef971dff56d6a29753f78fdac13a
rahulrajput83 <rahulrajput21112@gmail.com>
2022-11-11 16:41:42 +0530

    Updated

--- src/Components/Navbar/List.js
+++ src/Components/Navbar/List.js
@@ -1,14 +1,8 @@
 import { Link } from "react-router-dom";
-const List = ({ children }) => {
 
+const List = ({ children }) => {
     return (
-        <>
-            <ul className='ul'>
-                <li className='li'>
-                    <Link to="/" className='link' >{children}</Link>
-                </li>
-            </ul>
-        </>
+        <Link to="/" className='link' >{children}</Link>
     )
 }
 export default List;
