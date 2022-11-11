e462a9b4dc4fd23198bcd637c84e528eccc30942
rahulrajput83 <rahulrajput21112@gmail.com>
2022-11-11 17:28:27 +0530

    Link Cards

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 import HomePage from "./Components/HomePage/HomePage.js";
-import Navbar from './Components/Navbar/Navbar';
 import FoodDetails from './Components/FoodDetails/FoodDetails';
 
 
 function App() {
   return (
     <BrowserRouter >
-      <Navbar />
       <Routes>
         <Route path='/' element={<HomePage />} />
         <Route path='/agra' element={<FoodDetails />} />
