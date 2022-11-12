fe327524a8ed1eb653f9909b0b64eea9ac9fdc29
megha <meghamini40@gmail.com>
2022-11-12 12:20:06 +0530

    called different component

---
+++ src/Components/InvestorRelations/InvestorRelation.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Carousel from "./Carousel";
+import QuarterlyResult from "../IRQuarterlyResult/QR";
+import IRAboutUs from "../IRAboutUs/AboutUs";
+const InvestorRelation = () => {
+  return (
+    <div>
+      <Carousel />
+      <QuarterlyResult />
+      <IRAboutUs />
+    </div>
+  );
+};
+
+export default InvestorRelation;
