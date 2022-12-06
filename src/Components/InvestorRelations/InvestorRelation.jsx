dc8353e33f77a67603f1a614ff60a3e5d2639f01
megha <meghamini40@gmail.com>
2022-12-07 00:07:03 +0530

    include navbar and footer

--- src/Components/InvestorRelations/InvestorRelation.jsx
+++ src/Components/InvestorRelations/InvestorRelation.jsx
@@ -1,14 +1,19 @@
 import React from "react";
-import Carousel from "./Carousel";
+import Carousel from "../Carousel/Carousel";
 import QuarterlyResult from "../IRQuarterlyResult/QR";
 import IRAboutUs from "../IRAboutUs/AboutUs";
 import InvestorNavbar from "../InvestorNavbar/InvestorNavbar";
+import CorporateAnnouncements from "../CorporateAnnouncements/CorporateAnnouncements";
+import Footer from "../Footer/Footer";
 const InvestorRelations = () => {
   return (
     <>
+      <InvestorNavbar />
       <Carousel />
       <QuarterlyResult />
       <IRAboutUs />
+      <CorporateAnnouncements />
+      <Footer />
     </>
   );
 };
