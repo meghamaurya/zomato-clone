e7095701d5c9b7f9af0d526005ddeb330d745e00
megha <meghamini40@gmail.com>
2022-12-06 23:23:34 +0530

    fixed nabvar css

--- src/Components/InvestorNavbar/Title.jsx
+++ src/Components/InvestorNavbar/Title.jsx
@@ -5,7 +5,7 @@ const Title = () => {
       <Link to="/" className="add">
         <img
           src="https://b.zmtcdn.com/web_assets/b40b97e677bc7b2ca77c58c61db266fe1603954218.png?fit=around|198:42&crop=198:42;*,*"
-          width="120rem"
+          width="100rem"
           alt=""
         />
         <span className="heading">| Investor Relations</span>
