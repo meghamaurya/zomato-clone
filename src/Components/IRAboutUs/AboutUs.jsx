82059c06bdf4bbd1b9dfeaa9dc670b99518569c0
megha <meghamini40@gmail.com>
2022-12-11 15:33:43 +0530

    refactor code according to different screeen sizes

--- src/Components/IRAboutUs/AboutUs.jsx
+++ src/Components/IRAboutUs/AboutUs.jsx
@@ -5,6 +5,12 @@ const IRAboutUs = () => {
     <section className="section">
       <h2 className="heading">Who we are</h2>
       {/* <div className="description"> */}
+      <img
+        src="https://b.zmtcdn.com//data/o2_assets/e10ce1bb402e0a6451a0cfab2b3a67681654586354.png"
+        alt=""
+        width="100%"
+        className="image"
+      />
       <span className="para">
         Zomato’s mission is better food for more people. Started in 2010, Zomato
         offers services like restaurant search & discovery, reviews, home
@@ -17,12 +23,6 @@ const IRAboutUs = () => {
         opportunities to its delivery fleet, and contributing towards a more
         sustainable society through its non-profit entity Feeding India.
       </span>
-      <img
-        src="https://b.zmtcdn.com//data/o2_assets/e10ce1bb402e0a6451a0cfab2b3a67681654586354.png"
-        alt=""
-        width="110%"
-        className="image"
-      />
       {/* </div> */}
     </section>
   );
