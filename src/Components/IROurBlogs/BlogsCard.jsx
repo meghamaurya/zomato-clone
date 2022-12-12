230eb287195400810117575a6b353018031d8de6
megha <meghamini40@gmail.com>
2022-12-12 14:33:24 +0530

    added classnames

--- src/Components/IROurBlogs/BlogsCard.jsx
+++ src/Components/IROurBlogs/BlogsCard.jsx
@@ -1,21 +1,33 @@
 import React from "react";
 import BlogCard from "./BlogCard.json";
 import "./styles.css";
+import LinesEllipsis from "react-lines-ellipsis";
+import responsiveHOC from "react-lines-ellipsis/lib/responsiveHOC";
+
+const ResponsiveEllipsis = responsiveHOC()(LinesEllipsis);
 const BlogsCard = () => {
   return (
-    <div>
+    <div className="blogSection">
       {BlogCard.map((item) => {
         return (
           <div key={item.id} className="card">
             <img
               src={item.img}
               alt={item.title}
-              height="200px"
+              width="100%"
               className="cardImg"
             />
             <div className="detail">
               <span className="date">{item.date}</span>
-              <h3 className="title">{item.title}</h3>
+              <h3 className="title">
+                <ResponsiveEllipsis
+                  text={item.title}
+                  maxLine="1"
+                  ellipsis="..."
+                  trimRight
+                  basedOn="letters"
+                />
+              </h3>
               <p className="description">{item.description}</p>
             </div>
           </div>
--- src/Components/IROurBlogs/IROurBlogs.jsx
+++ src/Components/IROurBlogs/IROurBlogs.jsx
@@ -2,8 +2,8 @@ import BlogsCard from "./BlogsCard";
 
 const IROurBlogs = () => {
   return (
-    <div className="container">
-      <h2>From our blogs</h2>
+    <div className="blogContainer">
+      <h2 className="blogHeading">From our blogs</h2>
       <BlogsCard />
     </div>
   );
