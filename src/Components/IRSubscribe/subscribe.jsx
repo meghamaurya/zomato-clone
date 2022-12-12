cc92eb1059539b62b5804848ac205962f8344e7b
megha <meghamini40@gmail.com>
2022-12-12 16:14:55 +0530

    responsive design

--- src/Components/IRSubscribe/subscribe.jsx
+++ src/Components/IRSubscribe/subscribe.jsx
@@ -3,17 +3,21 @@ import "./styles.css";
 const Subscribe = () => {
   return (
     <div className="detailHolder">
-      <h2 className="title">Subscribe to our email alerts</h2>
-      <p className="info">Get regular updates through email.</p>
-      <input placeholder="Email" className="inputBox" />
-      <Button>Subscribe</Button>
-      <h2 className="title">Have questions?</h2>
-      <p className="info">
-        Reach out to us by emailing at
-        <span className="email"> meghamini40@gmail.com</span>, and we'll get
-        back to you.
-      </p>
-      <Button>Contact Us</Button>
+      <div className="part1">
+        <h2 className="title">Subscribe to our email alerts</h2>
+        <p className="info">Get regular updates through email.</p>
+        <input placeholder="Email" className="inputBox" />
+        <Button>Subscribe</Button>
+      </div>
+      <div className="part2">
+        <h2 className="title">Have questions?</h2>
+        <p className="info">
+          Reach out to us by emailing at
+          <span className="email"> meghamini40@gmail.com</span>, and we'll get
+          back to you.
+        </p>
+        <Button>Contact Us</Button>
+      </div>
     </div>
   );
 };
