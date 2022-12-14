7efc7fa5fe1ea11c8b5201c3f5866fb03aef91f8
Rahul Rajput <rahulrajput21112@gmail.com>
2022-12-14 15:57:01 +0530

    Fixed some issues on investor page

--- src/Components/InvestorNavbar/Title.jsx
+++ src/Components/InvestorNavbar/Title.jsx
@@ -1,7 +1,7 @@
 import { Link } from "react-router-dom";
 const Title = () => {
   return (
-    <>
+  
       <Link to="/" className="add">
         <img
           src="https://b.zmtcdn.com/web_assets/b40b97e677bc7b2ca77c58c61db266fe1603954218.png?fit=around|198:42&crop=198:42;*,*"
@@ -10,7 +10,7 @@ const Title = () => {
         />
         <span className="heading">| Investor Relations</span>
       </Link>
-    </>
+    
   );
 };
 
